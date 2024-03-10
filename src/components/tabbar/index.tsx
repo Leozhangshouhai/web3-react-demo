d168a07a729f4e0a216144dd77042aa32aa78578
zenghao1203 <810763928@qq.com>
2024-03-10 16:48:02 +0800

    feat: 更换任务图标

--- src/components/tabbar/index.tsx
+++ src/components/tabbar/index.tsx
@@ -1,6 +1,6 @@
 import home0 from "@/assets/home-0.png"
 import home1 from "@/assets/home-1.png"
-import task0 from "@/assets/task-0.png"
+import task0 from "@/assets/USDT.svg"
 import task1 from "@/assets/task-1.png"
 import { useTranslation } from 'react-i18next';
 import "./index.less"
@@ -19,7 +19,7 @@ export default function tabBar(){
         <div className="name">{t('首页')}</div>
       </div>
       <div className={`right item ${ !isActiveTask ?'':'item-active'}`}  onClick={() => { history.push('/task') }}> 
-        <img src={ isActiveTask ? task1 :task0} alt="" />
+        <img src={task0} alt="" />
         <div className="name">{t('任务')}</div>
        </div>
     </div>
