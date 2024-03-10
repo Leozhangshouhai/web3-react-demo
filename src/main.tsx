59bf759990f144b82dd086c10f4e903ffa801304
zenghao1203 <810763928@qq.com>
2024-03-10 21:04:32 +0800

    feat: 使用WebApp依赖

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import 'antd/dist/antd.css'; 
 import "./index.css";
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready();
 
 ReactDOM.render(<App />, document.getElementById("root"));
